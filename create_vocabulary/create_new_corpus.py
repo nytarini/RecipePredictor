7bd2791288634ac5f1c96e71e8bb7a07e828e4ac
mirawelner <miraewelner@gmail.com>
2020-08-01 17:21:38 -0700

    improved ingrediants

--- create_vocabulary/create_new_corpus.py
+++ create_vocabulary/create_new_corpus.py
@@ -2,10 +2,16 @@ original = open("kaggle_corpus.csv", "r")
 new = open("new_corpus.txt", "w")
 next(original)
 for line in original:
-    try:
-        line = line[line.index('Ingredients,"')+len('Ingredients,"'):]
+    if line.find('Ingredients,"') is not -1:
+        line = line[line.index('Ingredients,"') + len('Ingredients,"'):]
+    if line.find('"') is not -1:
         line = line[:line.index('"')]
-        new.write(line)
-    except:
-        continue
+    if line.find('Volume: 1') is not -1:
+        line = line[:line.index('Volume: 1')]
+    if line.find('Allergy Information:') is not -1:
+        line = line[:line.index('Allergy Information:')]
+    if line.find('Contains') is not -1:
+        line = line[:line.index('Contains')]
+    new.write(line + '\n')
+
 new.close()
